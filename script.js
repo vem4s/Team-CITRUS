c975feb244b00ab0f6868961018ebf17efd2fd2c
shockz92 <shockzoeriksson@gmail.com>
2025-08-09 15:51:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop - 60,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
